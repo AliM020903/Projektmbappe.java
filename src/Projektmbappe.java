84d0a52d846762a3221fe0d26bdf8034fbf02aec
AliTheBoss03 <96056143+AliTheBoss03@users.noreply.github.com>
2022-09-19 14:44:53 +0200

    Den orginale version

---
+++ src/Projektmbappe.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Projektmbappe {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a værdien er = ");
+        double a = scan.nextDouble();
+        System.out.println("b værdien er = ");
+        double b = scan.nextDouble();
+        System.out.println("x_0 værdien er = ");
+        double x_0 = scan.nextDouble();
+        System.out.println("y_0 værdien er = ");
+        double y_0 = scan.nextDouble();
+        System.out.println("Afstanden mellem linjen og punktet = ");
+        double Afstand = Math.abs(((a * x_0) - y_0 + b)) / (Math.sqrt(1 + (a * a)));
+
+        System.out.println(Afstand);
+    }
+
+}
+
